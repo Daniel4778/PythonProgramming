6a924a2644063bca3a78c9d46be85f9b063cc414
Daniel4778 <111440058+Daniel4778@users.noreply.github.com>
2022-08-22 06:57:51 -0700

    First Python Program

---
+++ first.py
@@ -0,0 +1,7 @@
+# Programmer : Tom with Jerry
+# Program : Hellow world; following the tradition
+# Version : Python 3.8.2
+# First written : 08-22-2022, 08:45:00 AM
+# Last Updated: 08-22-2022, 08:45:00 AM
+
+print("Hello world")
