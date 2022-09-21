cb64fa5e86821e4d2d8836bd0c5788f8b0f24531
Daniel Paddock <Daniel.Paddock@sckans.edu>
2022-09-21 06:45:21 -0700

    added alternate approach

--- oddNumber.py
+++ oddNumber.py
@@ -1,10 +1,14 @@
 # programmer: Daniel Paddock
 # program: Odd Numbers
 # created on: 09/21/2022 08:20:00 AM
-# last modified:
+# last modified: 0/21/2022 08:20:00 AM
 # version: 3.7.9
 
-print("Printing Odd Number from First 100 Natural Numbers")
-for i in range (0, 101):
-    if (i%2 != 0):
-        print(i, end=' ')
+# print("Printing Odd Number from First 100 Natural Numbers")
+# for i in range (0, 101):
+#     if (i%2 != 0):
+#         print(i, end=' ')
+
+print("Alternate Approach")
+for k in range(1, 101):
+    print(((2*k)-1), end=' ')
